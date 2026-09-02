1e5f312b83e0ba1060c6ef6c17f6c8f8bef7cd46
Carl Guinto <gmlolman400@gmail.com>
2026-09-02 23:24:09 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+
+const state = true;
+export default function Home() {
+  return (
+    <div>
+
+
+  <p className="text-3xl font-bold text-green-500">   
+      hello!
+    </p>
+    <Button> 
+      
+      CLICK 
+
+    </Button>
+  
+
+   </div>
+  )
+}
